3e9edbd2b7d180434c2871b44f58abc95cdef485
Zzai3242STPG <46597585+zzaizaiza@users.noreply.github.com>
2020-12-02 14:55:15 +0900

    2020/12/02Update- The WholeUnityProject
    Can:
    Walk, Run and Jump
    PickUp Grils
    Camera move with character

---
+++ ShareCtecture/Assets/PickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform theDest;
+
+    void OnMouseDown()
+    {
+        GetComponent<CapsuleCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = theDest.position;
+        this.transform.parent = GameObject.Find("PickUpItem").transform;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<CapsuleCollider>().enabled = true;
+        GetComponent<Rigidbody>().useGravity = true;
+        this.transform.parent = null;
+    }
+}
---
+++ ShareCtecture/Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = .4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    public float jumpHeight = 10f;
+
+    void Update() {
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            if (speed < 50) {
+                speed += 5f;
+                } else {
+                speed = 50;
+                }
+            }
+        if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            if (speed > 15) {
+                speed -= 5f;
+                } else {
+                speed = 15;
+                }
+            }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+            }
+
+        if (isGrounded && Input.GetButtonDown("Jump")) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+        }
+    }
